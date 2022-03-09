73fdc03dd48987bab3f1542cb65739466e266f9c
Monte Fischer <monte@montefischer.com>
2022-03-09 10:36:21 -0800

    Direct tabular implementation of MC RL

--- rl/monte/assignment11/tabular_rl.py
+++ rl/monte/assignment11/tabular_rl.py
@@ -1,3 +1,4 @@
+import itertools
 from typing import Iterator, Tuple, TypeVar, Callable, Iterable, Mapping, Sequence
 from operator import itemgetter
 import numpy as np
@@ -17,14 +18,25 @@ TabularValueFunctionApprox = Mapping[NonTerminal[S], float]
 
 def tabular_update(
         v: TabularValueFunctionApprox,
+        counts: Mapping[NonTerminal[S], int],
         steps: Iterable[ReturnStep[S]],
         count_to_weight_func: Callable[[int], float]
-) -> TabularValueFunctionApprox:
-    v_next: TabularValueFunctionApprox = v
-    for n, step in enumerate(steps):
-        state, reward = step.state, step.reward
-        v_next[state] = v[state] + count_to_weight_func(n) * (reward - v[state])
-    return v_next
+):
+    """
+    Perform a Monte Carlo update of the predicted value function of a FMRP given a
+    simulated trace.
+
+    Arguments:
+          v -- Current value function approximation (will be modified)
+          counts -- Dictionary of how many times state S has appeared in previous traces (will be modified)
+          steps -- iterator representing episode trace
+          count_to_weight_func -- learning rate parameter defined as a function of how many
+            times a state has been encountered previously
+    """
+    for step in steps:
+        state, reward = step.state, step.return_
+        counts[state] += 1
+        v[state] = v[state] + count_to_weight_func(counts[state]) * (reward - v[state])
 
 
 def tabular_mc_prediction(
@@ -33,17 +45,54 @@ def tabular_mc_prediction(
         gamma: float,
         episode_length_tolerance: float = 1e-6
 ) -> Iterator[TabularValueFunctionApprox[S]]:
+    """
+    Evaluate a finite Markov Reward Process using tabular Monte Carlo. Episodes
+    are simulated according to the given length tolerance. Yields approximations
+    of the value function for the FMRP after updating from an additional episode.
+
+    Arguments:
+        traces -- iterator of simultion traces from the FMRP
+        approx_0 -- initial value function approximation
+        gamma -- discount parameter for FMRP
+        episode_length_tolerance -- simulated episode ends when gamma^n <= tolerance
+
+    Returns an iterator of value functions representing successive updates with
+    the simulated episodes according to Monte Carlo MDP prediction.
+    """
     episodes: Iterator[Iterator[ReturnStep[S]]] = \
         (returns(trace, gamma, episode_length_tolerance) for trace in traces)
     f = approx_0
+    counts: Mapping[NonTerminal[S], int] = {s: 0 for s in f.keys()}
     yield f
 
     for episode in episodes:
-        f = tabular_update(f, episode, lambda n: 1./n)
+        tabular_update(f, counts, episode, lambda n: 1./n)
         yield f
+
 
 if __name__ == '__main__':
     # compare tabular-specific implementation with standard FunctionApprox
-    pass
+    from rl.chapter10.random_walk_mrp import RandomWalkMRP
+    from rl.chapter10.prediction_utils import fmrp_episodes_stream
+    import rl.iterate as iterate
+    from pprint import pprint
+    random_walk: RandomWalkMRP = RandomWalkMRP(
+        barrier=10,
+        p=0.5
+    )
+    num_episodes = 10000
+    gamma = 0.9
+    episodes: Iterable[Iterable[TransitionStep[int]]] = fmrp_episodes_stream(random_walk)
+    mc_vfs: Iterator[TabularValueFunctionApprox] = \
+        tabular_mc_prediction(
+            traces=episodes,
+            approx_0={s: 0.5 for s in random_walk.non_terminal_states},
+            gamma=gamma
+        )
+    final_mc_vf: TabularValueFunctionApprox = iterate.last(itertools.islice(mc_vfs, num_episodes))
+    print(f"Equal-Weights-MC Value Function with {num_episodes:d} episodes")
+    pprint({s: round(final_mc_vf[s], 3) for s in random_walk.non_terminal_states})
+    print("True Value Function")
+    random_walk.display_value_function(gamma=gamma)
 
 
