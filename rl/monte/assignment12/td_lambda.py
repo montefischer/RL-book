2faef5a7fe24c5563f4f95fbced279188d269d76
Monte Fischer <monte@montefischer.com>
2022-03-16 22:08:29 -0700

    Updates to td_lambda.py

--- rl/monte/assignment12/td_lambda.py
+++ rl/monte/assignment12/td_lambda.py
@@ -5,6 +5,8 @@ from rl.markov_process import NonTerminal, State, ReturnStep, TransitionStep
 from rl.returns import returns
 from rl.function_approx import learning_rate_schedule
 from rl.monte.assignment11.tabular_rl import extended_vf
+from rl.dynamic_programming import value_iteration_result
+from collections import defaultdict
 
 S = TypeVar('S')
 TabularValueFunctionApprox = Mapping[NonTerminal[S], float]
@@ -16,7 +18,8 @@ def tabular_td_lambda_update(
         trace: Iterable[TransitionStep],
         lambd: float,
         gamma: float,
-        learning_rate: Callable[[int], float]
+        learning_rate: Callable[[int], float],
+        num_episode_limit: int = 500
 ):
     """
     Update a tabular value function approximation using a trace experience using TD(lambda).
@@ -31,7 +34,7 @@ def tabular_td_lambda_update(
         learning_rate: Function to compute learning rate from number of times a state has
             been encountered across all trace experiences
     """
-    eligibility_traces: Mapping[NonTerminal[S], float] = {s: 0 for s in vf.keys()}
+    eligibility_traces: Mapping[NonTerminal[S], float] = defaultdict(float) #{s: 0 for s in vf.keys()}
     for t, transition_step in enumerate(trace):
         state, next_state, reward = transition_step.state, transition_step.next_state, transition_step.reward
         counts[state] += 1
@@ -41,6 +44,8 @@ def tabular_td_lambda_update(
             vf_s_update = (reward + gamma * extended_vf(vf, next_state) - vf[state]) * eligibility_traces[s]
             if counts[s] > 0:
                 vf[s] += learning_rate(counts[s]) * vf_s_update
+        if t == num_episode_limit:
+            break
 
 
 def tabular_td_lambda(
@@ -73,4 +78,39 @@ def tabular_td_lambda(
 
 
 if __name__ == '__main__':
-    pass
+    from rl.chapter3.simple_inventory_mdp_cap import SimpleInventoryMDPCap
+    from rl.distribution import Choose
+    from rl.policy import Policy
+    from dataclasses import dataclass
+    import numpy as np
+    mdp = SimpleInventoryMDPCap(2, 1.0, 1.0, 10.0)
+    gamma = 0.9
+    opt_vf_vi, opt_policy_vi = value_iteration_result(mdp, gamma=gamma)
+    print(opt_policy_vi)
+    print(opt_vf_vi)
+
+    uniform_dist = Choose(mdp.non_terminal_states)
+    print("options", uniform_dist.options)
+
+
+    A = TypeVar('A')
+    @dataclass(frozen=True)
+    class UniformPolicy(Policy[S, A]):
+        valid_actions: Callable[[S], Iterable[A]]
+
+        def act(self, state: NonTerminal[S]) -> Choose[A]:
+            return Choose(self.valid_actions(state))
+
+    print(mdp.mapping)
+    for vf_approx in tabular_td_lambda(
+        mdp.action_traces(Choose(mdp.non_terminal_states), UniformPolicy(mdp.actions)),
+        lambd=0.5,
+        initial_vf_approx={s: 0.0 for s in mdp.non_terminal_states},
+        gamma=gamma,
+        learning_rate=learning_rate_schedule(0.1, 10000, 0.5)
+    ):
+        error = 0
+        for s in mdp.non_terminal_states:
+            error += np.abs(opt_vf_vi[s] - vf_approx[s])
+        print(error)
+
